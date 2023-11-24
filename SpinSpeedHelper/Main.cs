1540a52778908d5770c1d733a7f9b1c3ea4ac0c3
Raoul1808 <raoulthegeek@gmail.com>
2023-11-24 19:27:46 +0100

    Initial Commit

---
+++ SpinSpeedHelper/Main.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+
+namespace SpinSpeedHelper
+{
+    [BepInPlugin(Guid, Name, Version)]
+    public class Main : BaseUnityPlugin
+    {
+        public const string Guid = "srxd.raoul1808.spinspeedhelper";
+        public const string Name = "Spin Speed Helper";
+        public const string Version = "0.1.0";
+
+        private void Awake()
+        {
+            Logger.LogMessage("Hi from Spin Speed Helper!");
+        }
+    }
+}

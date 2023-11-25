00a5c52c18c59bb6ad580f51fa0809d4eeff8141
Raoul1808 <raoulthegeek@gmail.com>
2023-11-25 16:18:13 +0100

    Bump version to 1.0.0

--- SpinSpeedHelper/Main.cs
+++ SpinSpeedHelper/Main.cs
@@ -12,7 +12,7 @@ namespace SpinSpeedHelper
     {
         public const string Guid = "srxd.raoul1808.spinspeedhelper";
         public const string Name = "Spin Speed Helper";
-        public const string Version = "0.1.0";
+        public const string Version = "1.0.0";
 
         private static ManualLogSource _logger;
 

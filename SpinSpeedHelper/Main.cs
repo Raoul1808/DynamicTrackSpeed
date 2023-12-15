2c7623f0238e53d03ce98194db93eaa35072908d
Raoul1808 <raoulthegeek@gmail.com>
2023-12-15 15:53:51 +0100

    Load speed triggers from srtb

--- SpinSpeedHelper/Main.cs
+++ SpinSpeedHelper/Main.cs
@@ -3,11 +3,13 @@ using System.IO;
 using System.Linq;
 using BepInEx;
 using BepInEx.Logging;
+using ChartHelper;
 using HarmonyLib;
 
 namespace SpinSpeedHelper
 {
     [BepInPlugin(Guid, Name, Version)]
+    [BepInDependency("srxd.raoul1808.charthelper")]
     public class Main : BaseUnityPlugin
     {
         public const string Guid = "srxd.raoul1808.spinspeedhelper";
@@ -29,6 +31,60 @@ namespace SpinSpeedHelper
 
         internal class QuickPatches
         {
+            struct CustomData
+            {
+                public string Name { get; set; }
+                public int Wysi { get; set; }
+            }
+
+            struct SpeedTrigger
+            {
+                public float Time { get; set; }
+                public float SpeedMultiplier { get; set; }
+                public bool InterpolateToNextTrigger { get; set; }
+            }
+
+            struct SpeedTriggersMetadata
+            {
+                public List<SpeedTrigger> Triggers { get; set; }
+            }
+
+            private static List<SpeedTrigger> TriggersFromSrtb(PlayableTrackData trackData)
+            {
+                var files = new List<IMultiAssetSaveFile>();
+                trackData.GetCustomFiles(files);
+                var file = files.First();
+                if (file is null) return null;
+                return CustomChartHelper.TryGetCustomData(file, "SpeedHelper_SpeedTriggers",
+                    out SpeedTriggersMetadata data) ? data.Triggers : null;
+            }
+
+            private static List<SpeedTrigger> TriggersFromSpeedsFile(string speedsPath)
+            {
+                var speeds = new List<SpeedTrigger>();
+                foreach (string line in File.ReadAllLines(speedsPath))
+                {
+                    var elems = line.Split(' '); 
+                    var trigger = new SpeedTrigger();
+                    if (elems.Length < 2) continue;
+                    if (float.TryParse(elems[0], out float time) && float.TryParse(elems[1], out float speed))
+                    {
+                        trigger.Time = time;
+                        trigger.SpeedMultiplier = speed;
+                    }
+                    else continue;
+
+                    if (elems.Length >= 3 && bool.TryParse(elems[2], out bool interpolate))
+                    {
+                        trigger.InterpolateToNextTrigger = interpolate;
+                    }
+
+                    speeds.Add(trigger);
+                }
+
+                return speeds;
+            }
+            
             [HarmonyPatch(typeof(SplineTrackData.DataToGenerate), MethodType.Constructor, typeof(PlayableTrackData)), HarmonyPostfix]
             public static void AddTrackSpeedsToSpline(SplineTrackData.DataToGenerate __instance, PlayableTrackData trackData)
             {
@@ -43,47 +99,29 @@ namespace SpinSpeedHelper
                 if (string.IsNullOrEmpty(customsDirectory))
                     return;
                 string speedsPath = Path.Combine(customsDirectory, speedsFilename);
-                var speeds = new List<(float, float, bool)>();
-                if (!File.Exists(speedsPath)) return;
-                foreach (string line in File.ReadAllLines(speedsPath))
-                {
-                    var elems = line.Split(' ');
-                    var trigger = (0f, 0f, false);
-                    if (elems.Length < 2) continue;
-                    if (float.TryParse(elems[0], out float time) && float.TryParse(elems[1], out float speed))
-                    {
-                        trigger.Item1 = time;
-                        trigger.Item2 = speed;
-                    }
-                    else continue;
+                var triggers = File.Exists(speedsPath)
+                    ? TriggersFromSpeedsFile(speedsPath)
+                    : TriggersFromSrtb(trackData);
 
-                    if (elems.Length >= 3 && bool.TryParse(elems[2], out bool interpolate))
-                    {
-                        trigger.Item3 = interpolate;
-                    }
-
-                    speeds.Add(trigger);
-                }
-
-                if (speeds.Count <= 0) return;
+                if (triggers == null || triggers.Count <= 0) return;
 
                 float initialSpeed = __instance.trackSpeeds[0].speed;
                 
                 // __instance.trackSpeeds.Clear();
-                foreach (var trigger in speeds)
+                foreach (var trigger in triggers)
                 {
                     __instance.trackSpeeds.Add(new TrackSpeedAtTime
                     {
-                        time = trigger.Item1,
-                        speed = trigger.Item2 * initialSpeed,
-                        interpolateToNextSpeed = trigger.Item3,
+                        time = trigger.Time,
+                        speed = trigger.SpeedMultiplier * initialSpeed,
+                        interpolateToNextSpeed = trigger.InterpolateToNextTrigger,
                     });
                 }
                 
                 if (__instance.trackTurns.Count == 1)
                     __instance.trackTurns.Add(new SplineRenderer.TrackTurnAndContext());
                 
-                Log($"Applied {speeds.Count} triggers from file {speedsFilename}");
+                Log($"Applied {triggers.Count} triggers from file {speedsFilename}");
             }
         }
     }

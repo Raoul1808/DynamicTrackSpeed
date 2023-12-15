e540380292383e34567c702fbdf218f1b6c913a5
Raoul1808 <raoulthegeek@gmail.com>
2023-12-15 19:18:07 +0100

    Fuse files together

--- srtb-integrate-speeds/src/lib.rs
+++ srtb-integrate-speeds/src/lib.rs
@@ -1,5 +1,14 @@
 use serde::{Deserialize, Serialize};
 
+pub type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
+
+#[macro_export]
+macro_rules! str_err {
+    ($e:expr) => {
+        Err($e.into())
+    };
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct RawSrtbFile {
@@ -32,4 +41,65 @@ pub struct LargeStringValuesContainer {
 pub struct LargeStringValue {
     pub key: String,
     pub val: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct SpeedTriggersData {
+    pub triggers: Vec<SpeedTrigger>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct SpeedTrigger {
+    pub time: f32,
+    pub speed_multiplier: f32,
+    pub interpolate_to_next_trigger: bool,
+}
+
+pub fn parse_speeds_file(content: String) -> Result<SpeedTriggersData> {
+    let mut triggers = Vec::new();
+    for line in content.lines().enumerate() {
+        let (line_number, line) = line;
+        let line = line.trim();
+        if line.is_empty() || line.starts_with('#') {
+            continue
+        }
+        let line: Vec<_> = line.trim().split_whitespace().collect();
+        if line.len() == 0 {
+            continue;
+        }
+        if line.len() < 2 || line.len() > 3 {
+            return str_err!(format!("Line {}: expected 2 or 3 values values, found {}", line_number, line.len()));
+        }
+        let time = line[0].parse();
+        let time: f32 = match time {
+            Ok(t) => t,
+            Err(_) => return str_err!(format!("Line {}: time value is not a valid number", line_number)),
+        };
+
+        let speed = line[1].parse();
+        let speed: f32 = match speed {
+            Ok(s) => s,
+            Err(_) => return str_err!(format!("Line {}: speed multiplier is not a valid number", line_number)),
+        };
+        
+        let interpolate = if line.len() != 3 { true } else {
+            let interpolate = line[2].parse();
+            match interpolate {
+                Ok(i) => i,
+                Err(_) => return str_err!(format!("Line {}: interpolation is not a valid boolean", line_number)),
+            }
+        };
+
+        let trigger = SpeedTrigger {
+            time,
+            speed_multiplier: speed,
+            interpolate_to_next_trigger: interpolate,
+        };
+        println!("Created trigger {:?}", trigger);
+        triggers.push(trigger);
+    }
+    let data = SpeedTriggersData { triggers };
+    Ok(data)
 }
--- srtb-integrate-speeds/src/main.rs
+++ srtb-integrate-speeds/src/main.rs
@@ -1,15 +1,7 @@
-use std::{fs, io};
+use std::fs;
 
 use rfd::FileDialog;
-use srtb_integrate_speeds::RawSrtbFile;
-
-type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
-
-macro_rules! str_err {
-    ($e:expr) => {
-        Err($e.into())
-    };
-}
+use srtb_integrate_speeds::*;
 
 fn program_flow() -> Result<()> {
     println!("Select a chart to open");
@@ -18,11 +10,37 @@ fn program_flow() -> Result<()> {
         .pick_file();
     let srtb_file = match file {
         Some(f) => f,
-        None => return str_err!("Please select a file"),
+        None => return str_err!("Please select a srtb file"),
+    };
+    let file = FileDialog::new()
+        .add_filter("Speed Triggers File", &["speeds"])
+        .pick_file();
+    let speeds_file = match file {
+        Some(f) => f,
+        None => return str_err!("Please select a speeds file"),
     };
     let chart_contents = fs::read_to_string(&srtb_file)?;
-    let chart: RawSrtbFile = serde_json::from_str(&chart_contents)?;
-    println!("{:#?}", chart);
+    let mut chart: RawSrtbFile = serde_json::from_str(&chart_contents)?;
+    let speeds_content = fs::read_to_string(&speeds_file)?;
+    let speeds = parse_speeds_file(speeds_content)?;
+    let speeds_json = serde_json::to_string(&speeds)?;
+    if let Some(value) = chart.large_string_values_container.values.iter_mut().find(|v| v.key == "SpeedHelper_SpeedTriggers") {
+        value.val = speeds_json.clone();
+    }
+    else {
+        chart.large_string_values_container.values.push(LargeStringValue { key: "SpeedHelper_SpeedTriggers".to_string(), val: speeds_json.clone() });
+    }
+    let chart = serde_json::to_string(&chart)?;
+    println!("Chart and speeds combined! Select which file the contents should be saved to");
+    let file = FileDialog::new()
+        .add_filter("Spin Rhythm Track Bundle", &["srtb"])
+        .save_file();
+    let dest_file = match file {
+        Some(f) => f,
+        None => return str_err!("Please select a destination file"),        
+    };
+    fs::write(dest_file, chart)?;
+    println!("All done!");
     Ok(())
 }
 
@@ -31,8 +49,11 @@ fn main() {
         Ok(()) => println!("Program finished executing without errors."),
         Err(e) => println!("{e}"),
     }
-    println!("Press ENTER to exit");
-    io::stdin()
-        .read_line(&mut String::new())
-        .expect("failed to read from stdin");
+    #[cfg(target_os = "windows")]
+    {
+        println!("Press ENTER to exit");
+        std::io::stdin()
+            .read_line(&mut String::new())
+            .expect("failed to read from stdin");
+    }
 }

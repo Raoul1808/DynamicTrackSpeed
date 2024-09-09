17438f93431ff761faee50a514848b0874547e4c
Raoul1808 <raoulthegeek@gmail.com>
2024-09-09 23:40:44 +0200

    Bump up version number

--- DynamicTrackSpeed/Main.cs
+++ DynamicTrackSpeed/Main.cs
@@ -18,7 +18,7 @@ namespace DynamicTrackSpeed
     {
         public const string Guid = "srxd.raoul1808.dynamictrackspeed";
         public const string Name = "Dynamic Track Speed";
-        public const string Version = "1.4.2";
+        public const string Version = "1.5.0";
 
         private static ManualLogSource _logger;
         private static CultureInfo _culture;
